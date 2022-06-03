c48d640b879366253ac851e57e40e12cc5ac815c
jonahchoi <cjonah227@gmail.com>
2022-06-03 01:16:55 -0700

    Added notification banner

---
+++ src/components/Banner.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { Alert } from 'react-bootstrap'
+
+const Banner = ({showBanner, setShowBanner, bannerPhrase}) => {
+  
+  useEffect(()=>{
+    if(showBanner){
+      const timer = setTimeout(()=>{
+        setShowBanner(false);
+      }, 5000)
+
+      return ()=>{
+        clearTimeout(timer)
+      }
+    }
+  },[showBanner])
+
+  return (
+    <div className='fixed-top' >
+      <Alert 
+        show={showBanner}
+        onClose={()=>setShowBanner(false)} 
+        dismissible 
+        variant={bannerPhrase === 'Password copied successfully!' ? 'success' : 'warning'}>
+          {bannerPhrase}</Alert>
+      
+    </div>
+  )
+}
+
+export default Banner
